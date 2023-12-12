ac0cb9e90b8c430a4b4a4ad4e03286aaeee20ba9
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-12 15:53:13 +0900

    feat: csrfTokenをaxiosにセットする

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Auth, Todo } from "./pages"
+import { useCsfrToken } from "./hooks/useCsrfToken"
+import axios from "axios"
 
 const App: FC = () => {
+  const { csrfToken } = useCsfrToken()
+
+  useEffect(() => {
+    if (!csrfToken) return
+    axios.defaults.withCredentials = true
+    axios.defaults.headers.common["X-CSRF-Token"] = csrfToken
+  }, [csrfToken])
+
   return (
     <BrowserRouter>
       <Routes>
--- src/hooks/useCsrfToken.ts
+++ src/hooks/useCsrfToken.ts
@@ -1,14 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 import { CsrfToken } from "../types"
 
 export const useCsfrToken = () => {
+  const [csrfToken, setCsrfToken] = useState<CsrfToken["csrf_token"]>()
   useEffect(() => {
-    axios.defaults.withCredentials = true
-    const getCsfrToken = async () => {
-      const { data } = await axios.get<CsrfToken>(`${process.env.API_URL}/csrf`)
-      axios.defaults.headers.common["X-CSRF-Token"] = data.csrf_token
+    if (!csrfToken) {
+      const getCsfrToken = async () => {
+        const { data } = await axios.get<CsrfToken>(
+          `${process.env.API_URL}/csrf`
+        )
+        setCsrfToken(data.csrf_token)
+      }
+      getCsfrToken()
     }
-    getCsfrToken()
-  }, [])
+  }, [csrfToken])
+
+  return { csrfToken }
 }
