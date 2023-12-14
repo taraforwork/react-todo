36cc2ed2f0ebf07fba627827a4b55967b8f16791
taraforwork <miyazaki.tara.for.work@gmail.com>
2023-12-14 11:34:40 +0900

    feat: apiUrlを定数化

--- src/hooks/useCsrfToken.ts
+++ src/hooks/useCsrfToken.ts
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { CsrfToken } from "../types"
+import { apiUrl } from "../utils/const"
 
 export const useCsrfToken = () => {
   const [csrfToken, setCsrfToken] = useState<CsrfToken["csrf_token"]>()
   useEffect(() => {
     if (!csrfToken) {
       const getCsfrToken = async () => {
-        const { data } = await axios.get<CsrfToken>(
-          `${import.meta.env.VITE_API_URL}/csrf`
-        )
+        const { data } = await axios.get<CsrfToken>(`${apiUrl}/csrf`)
         setCsrfToken(data.csrf_token)
       }
       getCsfrToken()
